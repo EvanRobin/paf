f09cfdd86a8b88e075271748de6ce24a3d0f7732
EvanRobin <100917489+EvanRobin@users.noreply.github.com>
2022-05-08 22:48:17 +0200

    Add files via upload

--- paf/Vjezbe7/Particle.h
+++ paf/Vjezbe7/Particle.h
@@ -1,6 +1,6 @@
+#include <cmath>
 class Particle {
     private:
-
     double t, x, y, vx, vy;
     double dt;
     double g = -9.81;
@@ -8,7 +8,7 @@ class Particle {
     void evolve();
 
     public:
-    Particle(double v, double theta, double x0, double y0, double step=0.001);
+    Particle(double v, double angle, double x0, double y0, double step=0.001);
     ~Particle();
 
     double range();
