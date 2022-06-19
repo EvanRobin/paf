2c6dfd96845e959a49942a81bdf218ec133c2ae3
EvanRobin <100917489+EvanRobin@users.noreply.github.com>
2022-06-19 13:40:58 +0200

    Delete Vjezbe2 directory

--- Vjezbe2/zad3.py
+++
@@ -1,4 +0,0 @@
-import Kinematika as kn
-
-kn.jednoliko_gibanje(10,100,45,1,10)
-kn.kosi_hitac(10,100,45,1,10)
--- Vjezbe2/zad4.py
+++
@@ -1,8 +0,0 @@
-import zad4modul as z4
-z4.kosi(100,45)
-
-z4.max(100,45)
-
-z4.domet(100,45)
-
-z4.max_speed(100,45)
